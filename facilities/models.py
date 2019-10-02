be1ea871e9703429c9cfc0772a9911c035caf883
Simon <simonmarkholland@gmail.com>
2019-10-02 14:18:44 +0100

    Add facilities models

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+
+class FacilitiesIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        facilitiespages = self.get_children().live().order_by('-first_published_at')
+        context['facilitiespages'] = facilitiespages
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+
+
+class FacilitiesPage(Page):
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
