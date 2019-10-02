94938b9b031116536fd3b7eedbf00a1ecfe8d572
Simon <simonmarkholland@gmail.com>
2019-10-02 14:45:28 +0100

    Add images into the facilities app

--- facilities/models.py
+++ facilities/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 class FacilitiesIndexPage(Page):
@@ -20,10 +23,16 @@ class FacilitiesIndexPage(Page):
     ]
 
 
-
 class FacilitiesPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -33,4 +42,17 @@ class FacilitiesPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class FacilitiesPageGalleryImage(Orderable):
+    page = ParentalKey(FacilitiesPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
