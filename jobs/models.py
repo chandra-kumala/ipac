8321b5d58b31431a1c12559e55eb5938672bb563
Simon <simonmarkholland@gmail.com>
2019-10-03 11:10:50 +0100

    Add the jobs models and templates

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+
+import datetime
+
+class JobsIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        jobspages = self.get_children().live().order_by('-first_published_at')
+        context['jobspages'] = jobspages
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class JobsPage(Page):
+    date = models.DateField(("Post date"), default=datetime.date.today)
+
+    intro = models.CharField(max_length=250)
+    start_date = models.CharField(max_length=150)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('start_date'),
+        FieldPanel('body', classname="full"),
+    ]
