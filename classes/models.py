ff6725a9068b9d5584ca7271e6957cac76666409
Simon <simonmarkholland@gmail.com>
2019-10-03 14:51:08 +0100

    Add classes page model

--- classes/models.py
+++ classes/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class ClassesIndexPage(Page):
@@ -8,4 +11,19 @@ class ClassesIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+
+class ClassesPage(Page):
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
     ]
