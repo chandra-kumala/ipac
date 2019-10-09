a61e53f853bbdc00dc76a91b03b8fe7d31eb7162
Simon <simonmarkholland@gmail.com>
2019-10-09 16:59:39 +0100

    Create GenericPage model and template

--- home/models.py
+++ home/models.py
@@ -2,9 +2,27 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
+from wagtail.embeds.blocks import EmbedBlock
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
+
+class GenericPage(Page):
+    template = "home/generic_page.html"
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ])
+    
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+    class Meta:
+        verbose_name = "Generic Page"
+        verbose_name_plural = "Generic Pages"
+
 
 class HomePage(Page):
     parent_page_types = ['wagtailcore.page']
