edc9c7fd7451bc6bb35985cd77d27702f736b681
Simon <simonmarkholland@gmail.com>
2019-10-11 14:53:12 +0100

    Initial attempt at handling search engine meta data
    Simply creating and uploading fields for basic SEO

--- home/models.py
+++ home/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.core import blocks
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
 
 class GenericPage(Page):
     template = "home/generic_page.html"
+    
+    desc = models.CharField("Description shown on Social Media", max_length=255, null=True, blank=True)
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
@@ -17,6 +22,8 @@ class GenericPage(Page):
     ])
     
     content_panels = Page.content_panels + [
+        FieldPanel('desc'),
+        InlinePanel('desc_images', label="Social Image"),
         StreamFieldPanel('body'),
     ]
     class Meta:
@@ -28,6 +35,7 @@ class HomePage(Page):
     parent_page_types = ['wagtailcore.page']
     subpage_types = ['classes.Classes', 'jobs.Jobs', 'facilities.Facilities', 'home.GenericPage']
 
+    desc = models.CharField("Description shown on Social Media", max_length=255, null=True, blank=True)
     body = StreamField([
         ('heading', blocks.CharBlock(classname="full title")),
         ('paragraph', blocks.RichTextBlock()),
@@ -36,9 +44,21 @@ class HomePage(Page):
     ])
 
     content_panels = Page.content_panels + [
+        FieldPanel('desc'),
+        InlinePanel('desc_images', label="Social Image"),
         StreamFieldPanel('body'),
     ]
 
     class Meta:
         verbose_name = "Home Page"
         verbose_name_plural = "Home Pages"
+
+class DescImage(Orderable):
+    page = ParentalKey(Page, on_delete=models.CASCADE, related_name='desc_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True, on_delete=models.CASCADE, related_name='+'
+    )
+
+    panels = [
+        ImageChooserPanel('image'),
+    ]
