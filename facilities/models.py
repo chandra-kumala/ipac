0beebca90ae73b60001c4aa26c1f950243d9a837
Simon <simonmarkholland@gmail.com>
2019-10-15 13:45:01 +0100

    Import MultiFieldPanel into app models

--- facilities/models.py
+++ facilities/models.py
@@ -4,7 +4,7 @@ from modelcluster.fields import ParentalKey
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
